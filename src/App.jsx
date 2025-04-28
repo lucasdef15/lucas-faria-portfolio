68990cd0b6100d40a63f77f8f6ee687ddba93ce2
lucasdef15 <lucasff15@hotmail.com>
2025-04-28 14:30:21 -0300

    modified:   src/App.jsx 	new file:   src/components/LogoSection.jsx 	new file:   src/components/NavBar.jsx 	new file:   src/components/sections/ShowcaseSection.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "./components/sections/Hero";
+import ShowcaseSection from "./components/sections/ShowcaseSection";
+import NavBar from "./components/NavBar";
+import LogoSection from "./components/LogoSection";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
+      <ShowcaseSection />
+      <LogoSection />
     </>
   );
 };
